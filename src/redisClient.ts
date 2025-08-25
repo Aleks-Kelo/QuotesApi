d116affc298d57ad439c10b6b8c45e72bedd5466
A.KELO <aleksandro.kelo@lhind.dlh.de>
2025-08-25 04:48:34 +0200

    reverting shit

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -4,6 +4,7 @@ import { createClient } from 'redis';
 const redisClient = createClient({
     url: process.env.REDIS_URL,
     password: process.env.REDIS_PASSWORD,
+    socket: { tls: true },
 });
 
 redisClient.on('error', (err) => console.error('Redis error:', err));
