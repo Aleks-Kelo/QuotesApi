9fff59dfee9e91cdae58b90e6e2cf179b7d25186
A.KELO <aleksandro.kelo@lhind.dlh.de>
2025-08-25 04:39:46 +0200

    idk

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -4,7 +4,6 @@ import { createClient } from 'redis';
 const redisClient = createClient({
     url: process.env.REDIS_URL,
     password: process.env.REDIS_PASSWORD,
-    socket: { tls: true },
 });
 
 redisClient.on('error', (err) => console.error('Redis error:', err));
